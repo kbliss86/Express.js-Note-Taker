a6e34f8f51a71718e4633cad6466627f67492627
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-08-18 01:53:55 -0500

    added code for serverjs file and indexjs

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const api = require('./routes/index');
+
+const app = express();
+
+const port = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.use('/api', api);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+)
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+)
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`)
+);
+
